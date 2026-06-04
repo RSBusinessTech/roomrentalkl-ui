c70abc049cb43422bf3831d5cf3747ff8f138e1d
RS BusinessTech <rsbusinesstech@gmail.com>
2026-06-04 21:54:45 +0530

    adding hero for home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  ngOnInit(): void {
+    this.checkReveal();
+  }
 
-  ngOnInit() {
+  @HostListener('window:scroll')
+  onScroll() {
+    this.checkReveal();
+  }
+
+  checkReveal() {
+    const elements = document.querySelectorAll('.reveal');
+    const windowHeight = window.innerHeight;
+
+    elements.forEach((el: any) => {
+      const position = el.getBoundingClientRect().top;
+
+      if (position < windowHeight - 100) {
+        el.classList.add('active');
+      }
+    });
   }
 
 }
