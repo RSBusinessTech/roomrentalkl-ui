c69e4514018fd8d96668c1ba96e4994a0e31ba0f
RS BusinessTech <rsbusinesstech@gmail.com>
2026-06-04 19:37:39 +0530

    adding missing code

--- src/app/features/shared/header/header.component.ts
+++ src/app/features/shared/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+   menuOpen = false;
+
    openRooms = false;
    openProperty = false;
    openTransit = false;
