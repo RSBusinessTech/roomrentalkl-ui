fb2e3cabf5a2afcafcaef15fb41f7538bf45c92b
RS BusinessTech <rsbusinesstech@gmail.com>
2026-06-15 00:05:24 +0530

    adding comparison

--- src/app/models/nearbyArea.ts
+++ src/app/models/nearbyArea.ts
@@ -1,5 +1,17 @@
 export interface NearbyArea {
+
   name: string;
+
   slug: string;
+
   averageRent: number;
+
+  mrtAccess: boolean;
+
+  distanceFromKLCC: number;
+
+  popularFor: string;
+
+  description: string;
+
 }
--- src/app/shared/components/compare-areas/compare-areas.component.ts
+++ src/app/shared/components/compare-areas/compare-areas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-compare-areas',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompareAreasComponent implements OnInit {
 
+  @Input() areas: any[] = [];
+  @Input() area: any;
+  showAll: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  // Remove current area from comparison
+  get filteredAreas() {
+    return this.areas.filter(
+      item => !this.area || item.name !== this.area.name
+    );
+  }
+
+
+  // Show first 5 areas initially
+  // Show all after clicking View All
+  get displayedAreas() {
+    const areas = this.filteredAreas;
+    return this.showAll
+      ? areas
+      : areas.slice(0, 5);
+  }
+
+
+  // Toggle View All / Show Less
+  toggleViewAll() {
+    this.showAll = !this.showAll;
+  }
 }
